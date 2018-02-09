40f89b10e40bbd36d0a1791fa204158c42cd3028
Antonio <antonioallis@hotmail.co.uk>
2018-02-09 22:58:16 +0000

    Fix so many bugs

--- cipher.py
+++ cipher.py
@@ -3,7 +3,7 @@ plaintext = input("Input text: ")
 key = int(input("key (0 - 25): "))
 ciphertext = ""
 
-"".join(plaintext.split())
+plaintext = "".join(plaintext.split())
 plaintext = plaintext.lower()
 
 assert key < 26 and key >= 0, "Key is invalid"
